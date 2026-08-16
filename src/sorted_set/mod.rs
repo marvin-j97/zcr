c2b2f82a0e0169ca449c0a2dff651269489ef9cf
marvin-j97 <marvin.janke.97@gmail.com>
2026-08-16 14:00:24 +0200

    add sorted string set

---
+++ src/sorted_set/mod.rs
@@ -0,0 +1,111 @@
+mod builder;
+mod mutator;
+
+use crate::{BorrowedList, OwnedList, ValueAccessor, sorted_set::mutator::Mutator};
+
+pub use builder::SortedSetBuilder;
+
+/// A zero-copy, sorted byte set borrowing a byte slice
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct BorrowedSortedSet<'a> {
+    inner: BorrowedList<'a>,
+}
+
+impl<'a> BorrowedSortedSet<'a> {
+    pub fn from_slice(slice: &'a [u8]) -> Self {
+        Self {
+            inner: BorrowedList::from_slice(slice),
+        }
+    }
+
+    pub fn mutate(&'a self, f: impl FnOnce(&mut Mutator<'a>) -> ()) -> OwnedSortedSet {
+        let mut m = Mutator::new(self);
+        f(&mut m);
+        m.finish()
+    }
+
+    pub fn as_list(&self) -> BorrowedList<'a> {
+        self.inner.clone()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = ValueAccessor<'_>> {
+        self.inner.iter()
+    }
+
+    pub fn has<'b>(&self, key: &[u8]) -> bool {
+        self.inner
+            .binary_search_by(|v| v.as_bytes().expect("should be bytes").cmp(key))
+            .is_ok()
+    }
+
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+/// A zero-copy, sorted byte set that owns its byte buffer
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct OwnedSortedSet {
+    inner: OwnedList,
+}
+
+impl OwnedSortedSet {
+    pub(crate) fn new(list: OwnedList) -> Self {
+        Self { inner: list }
+    }
+
+    pub fn as_borrowed(&self) -> BorrowedSortedSet<'_> {
+        BorrowedSortedSet {
+            inner: self.inner.as_borrowed(),
+        }
+    }
+
+    pub fn as_list(&self) -> BorrowedList<'_> {
+        self.inner.as_borrowed()
+    }
+
+    pub fn into_list(self) -> OwnedList {
+        self.inner
+    }
+
+    pub fn len(&self) -> usize {
+        self.as_borrowed().len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = ValueAccessor<'_>> {
+        self.inner.iter()
+    }
+
+    pub fn has<'b>(&self, key: &[u8]) -> bool {
+        self.as_borrowed().has(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::SortedSetBuilder;
+    use test_log::test;
+
+    #[test]
+    fn sorted_set_smoke_test() {
+        let mut set = SortedSetBuilder::default();
+        set.insert(b"hello");
+        set.insert(b"bye");
+        set.insert(b"whatever");
+        let set = set.finish();
+
+        assert_eq!(3, set.len(), "should have 3 items");
+
+        assert!(set.has(b"hello"), "should have item");
+        assert!(set.has(b"bye"), "should have item");
+        assert!(set.has(b"whatever"), "should have item");
+
+        assert!(!set.has(b"nonexistent"), "should not have item");
+
+        {
+            let it = set.iter().map(|v| v.as_bytes().unwrap());
+            let items = it.collect::<Vec<_>>();
+            assert_eq!(vec![b"bye" as &[u8], b"hello", b"whatever"], items);
+        }
+    }
+}
